572fc635373c97b67d56a036ea340ddd209bbc38
SteamPako <geekerjune@gmail.com>
2016-11-25 22:07:09 +0800

    [Info]WeChat-leason

---
+++ wxapp-004/pages/index/index.js
@@ -0,0 +1,53 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    text: 'init data',
+    array: [{text: 'init data'}],
+    object: {
+      text: 'init data'
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    //console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  changeText: function() {
+    // this.data.text = 'changed data'  // bad, it can not work
+    this.setData({
+      text: 'changed data'
+    })
+  },
+  changeItemInArray: function() {
+    // you can use this way to modify a danamic data path
+    this.setData({
+      'array[0].text':'changed data'
+    })
+  },
+  changeItemInObject: function(){
+    this.setData({
+      'object.text': 'changed data'
+    });
+  },
+  addNewField: function() {
+    this.setData({
+      'newField.text': 'new data'
+    })
+  }
+})
